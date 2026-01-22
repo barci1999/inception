02e2f7357f65ea5a355f1939982aa8bf4d807ad2
pablalva <pablalva@prueba.myguest.virtualbox.org>
2026-01-22 19:46:44 +0100

    se abre la pagina de configuracion de wordpress

--- docker-inception/src/requirements/mariadb/tools/entrypoint.sh
+++ docker-inception/src/requirements/mariadb/tools/entrypoint.sh
@@ -24,14 +24,15 @@ echo "MariaDB is ready!"
 
 # Run setup SQL: create database and users
 echo "Running setup SQL..."
-mysql --socket=/run/mysqld/mysqld.sock -u root << EOF
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+mysql --socket=/run/mysqld/mysqld.sock \
+  -u root \
+  -p"$(cat /run/secrets/db_root_password)" << EOF
+
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '$(cat /run/secrets/db_password)';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-
 # Shut down temporary server
 echo "Shutting down temporary MariaDB..."
 mysqladmin --socket=/run/mysqld/mysqld.sock -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
